28a4e8594cecbd85966783713c72b04886dc35b0
Jerry Chang <fx2ptt@gmail.com>
2025-11-10 21:42:05 +0800

    新增 檢核邏輯

--- src/views/products/AddProduct.vue
+++ src/views/products/AddProduct.vue
@@ -20,7 +20,7 @@
       </el-form-item>
 
       <el-form-item label="庫存" prop="stock">
-        <el-input-number v-model="form.stock" :min="0" :step="100" />
+        <el-input-number v-model="form.stock" :min="0" :step="1" :placeholder="'請輸入庫存'" />
       </el-form-item>
 
       <el-form-item label="描述">
@@ -72,6 +72,10 @@ const rules = {
   name: [{ required: true, message: '請輸入商品名稱', trigger: 'blur' }],
   category: [{ required: true, message: '請選擇分類', trigger: 'change' }],
   price: [{ required: true, message: '請輸入價格', trigger: 'blur' }],
+  stock: [
+    { required: true, message: '請輸入庫存', trigger: 'blur' },
+    { type: 'number', min: 0, message: '庫存數量不能為負數', trigger: 'blur' },
+  ],
   imageBase64: [{ required: true, message: '請上傳圖片', trigger: 'change' }],
 }
 
@@ -138,7 +142,8 @@ function removeImage() {
 function resetForm() {
   form.name = ''
   form.category = ''
-  form.price = null
+  form.price = 1000
+  form.stock = 10
   form.description = ''
   form.imageBase64 = ''
   imagePreview.value = null
