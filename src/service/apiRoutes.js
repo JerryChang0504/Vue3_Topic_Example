dc20d374becd0bdefacad099c5975c0fc0a45ecf
DESKTOP-P4UUKAO\student <redcarbb@gmail.com>
2025-11-10 20:57:02 +0800

    Delete API

--- src/service/apiRoutes.js
+++ src/service/apiRoutes.js
@@ -9,4 +9,6 @@ export const API_ROUTES = {
   PRODUCT_LIST: '/api/products',
   PRODUCT_DETAIL: (id) => `/api/products/edit/${id}`,
   PRODUCT_UPDATE: (id) => `/api/updateProducts/${id}`,
+  PRODUCT_MANAGE: '/api/products/manage',
+  PRODUCT_DELETE: (id) => `/api/deleteProducts/${id}`,
 }
