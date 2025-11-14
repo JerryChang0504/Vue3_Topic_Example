fee4a40c12770927bf106cdd307b3204571f1e87
Evan9399 <kiritoku8299@gmail.com>
2025-11-14 21:15:00 +0800

    Revert "no message"
    This reverts commit e39b3cab0c9d439b9d44203131ed08bbb0aa3415.

--- src/views/products/ProductList.vue
+++ src/views/products/ProductList.vue
@@ -186,9 +186,9 @@ onMounted(async () => {
 
 const AddOption = () => {
   api.addOption({
-    // listName: 'ProductClass',
-    // name: '電子產品',
-    // value: '001',
+    listName: 'ProductClass',
+    name: '電子產品',
+    value: '001',
     sortOrder: 1,
     isActive: true,
     description: '電子產品類別',
