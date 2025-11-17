fea65bdba8cb514d82779e5dca775d62025f4098
Evan9399 <kiritoku8299@gmail.com>
2025-11-17 21:53:22 +0800

    選項存進 session Storage

--- src/main.js
+++ src/main.js
@@ -1,17 +1,19 @@
 // import './assets/main.css'
 
-import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 import App from './App.vue'
 import AlertBox from './components/AlertBox.vue'
 import FormatPlugin from './plugins/format' // 載入插件
 import router from './router'
-import { createPinia } from 'pinia'
 import { formatSecondsToHHMMSS } from './utils/format'
 const pinia = createPinia()
 
 const app = createApp(App)
+const res = await getAndCacheOptions()
+console.log('🚀 ~ res:', res)
 app.config.globalProperties.$formatSecondsToHHMMSS = formatSecondsToHHMMSS
 app.use(FormatPlugin) // 註冊插件
 app.use(pinia)
