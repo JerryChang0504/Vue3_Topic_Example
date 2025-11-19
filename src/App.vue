9ddae32b86a0faa4763dbff49e44ed84b3788049
DESKTOP-P4UUKAO\student <kim1475369h@gmail.com>
2025-11-19 22:17:14 +0800

    新增 權限設置

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,19 @@
 <script setup>
-import { Coffee, Cpu, Flag, Monitor, Odometer, Suitcase } from '@element-plus/icons-vue'
-import Breadcrumb from './Navigation/Breadcrumb.vue'
-import CategorySidebar from './Navigation/CategorySidebar.vue'
-import TopBar from './Navigation/TopBar.vue'
-import api from './service/api'
+import Breadcrumb from '@/Navigation/Breadcrumb.vue'
+import CategorySidebar from '@/Navigation/CategorySidebar.vue'
+import TopBar from '@/Navigation/TopBar.vue'
+import { computed } from 'vue'
+import { getNavMenu } from './Navigation/getNavMenu'
+import { useUserStore } from './store/userStore'
 
-import { onMounted } from 'vue'
-
-const getusers = async () => {
-  const res = await api.user()
-  console.log('🚀 ~ res:', res?.result)
-  return res
-}
-
-onMounted(() => {
-  getusers()
+const userStore = useUserStore()
+const userRole = computed(() => {
+  return userStore.userRole
 })
-
-const categories = [
-  {
-    name: 'products',
-    label: '商品相關',
-    icon: Monitor,
-    clickable: false,
-    subs: [
-      { name: '', label: '商品介紹', icon: Cpu },
-      { name: 'setting', label: '商品管理', icon: Cpu },
-    ],
-  },
-  {
-    name: 'settings',
-    label: '管理相關',
-    icon: Odometer,
-    clickable: false,
-    subs: [{ name: 'options', label: '選項管理', icon: Cpu }],
-  },
-  {
-    name: 'life',
-    label: '生活類',
-    icon: Coffee,
-    clickable: false,
-    subs: [
-      { name: 'food', label: '美食', icon: Coffee },
-      {
-        name: 'travel',
-        label: '旅遊',
-        icon: Suitcase,
-        clickable: false,
-        subs: [
-          { name: 'usa', label: '美國', icon: Flag },
-          { name: 'canada', label: '加拿大', icon: Flag },
-        ],
-      },
-    ],
-  },
-]
+console.log('🚀 ~ userRole:', userRole)
+const navMenu = computed(() => {
+  return getNavMenu(userRole.value)
+})
 </script>
 
 <template>
@@ -64,10 +23,10 @@ const categories = [
 
   <main>
     <div class="layout">
-      <CategorySidebar :categories="categories" />
+      <CategorySidebar :categories="navMenu" />
 
       <div class="content">
-        <Breadcrumb :categories="categories" />
+        <Breadcrumb :categories="navMenu" />
         <router-view />
       </div>
     </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import CategoryPage from '@/Navigation/sub/CategoryPage.vue'
+import Storage, { USER_ROLE_KEY } from '@/utils/storageUtil'
 import About from '@/views/About.vue'
 import UserPost from '@/views/UserPost.vue'
+
 import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   { path: '/', name: 'Home', component: () => import('@/views/Home.vue') },
@@ -22,11 +24,7 @@ const routes = [
     name: 'AddProduct',
     component: () => import('@/views/products/AddProduct.vue'),
   },
-  {
-    path: '/products/setting',
-    name: 'ProductSetting',
-    component: () => import('@/views/products/ProductManage.vue'),
-  },
+
   {
     path: '/products/edit/:id',
     name: 'EditProduct',
@@ -36,9 +34,21 @@ const routes = [
     path: '/settings/options',
     name: 'SettingsOptions',
     component: () => import('@/views/settings/OptionsManage.vue'),
+    meta: { requiresAuth: true, role: ['ADMIN'] },
+  },
+  {
+    path: '/settings/setting',
+    name: 'ProductSetting',
+    component: () => import('@/views/products/ProductManage.vue'),
+    meta: { requiresAuth: true, role: ['ADMIN'] },
   },
   { path: '/about', name: 'About', component: About },
   { path: '/users/:username/posts/:postId', component: UserPost },
+  {
+    path: '/accessDenied',
+    name: 'AccessDenied',
+    component: () => import('@/views/users/AccessDenied.vue'),
+  },
   {
     path: '/:paths(.*)*',
     name: 'Category',
@@ -54,11 +64,15 @@ const router = createRouter({
 // ✅ 加入全域導航守衛：權限驗證
 router.beforeEach((to, from, next) => {
   const isLoggedIn = !!localStorage.getItem('token')
-
+  const role = Storage.get(USER_ROLE_KEY)
+  console.log('🚀 ~ role:', role)
+  console.log('🚀 ~ to.meta:', to.meta)
   if (to.meta.requiresAuth && !isLoggedIn) {
     return next('/login')
   }
-
+  if (to.meta.requiresAuth && !to.meta.role.includes(role)) {
+    return next('/accessDenied')
+  }
   next()
 })
 export default router
